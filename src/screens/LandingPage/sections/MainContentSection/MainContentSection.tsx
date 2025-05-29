ed3bafd9c7afdb6ff3f7f35b9dcce7d56f02d2d1
Sushant <sushantbhagat561@gmail.com>
2025-05-29 20:03:55 +0530

    .

--- src/screens/LandingPage/sections/MainContentSection/MainContentSection.tsx
+++ src/screens/LandingPage/sections/MainContentSection/MainContentSection.tsx
@@ -8,9 +8,9 @@ export const MainContentSection = (): JSX.Element => {
       <div className="flex flex-col items-start justify-center gap-6 relative flex-1 self-stretch w-full md:max-w-[50%] px-6 md:px-0 order-2 md:order-1 pb-12 md:pb-0">
         <div className="flex flex-col items-start gap-6 relative self-stretch w-full">
           <div className="flex flex-col items-start gap-2 relative self-stretch w-full">
-            <h1 className="relative self-stretch mt-[-1.00px] font-heading-1-m font-[number:var(--heading-1-m-font-weight)] text-white text-[length:var(--heading-1-m-font-size)] tracking-[var(--heading-1-m-letter-spacing)] leading-[1.4] md:leading-[var(--heading-1-m-line-height)] [font-style:var(--heading-1-m-font-style)] text-sm md:text-5xl font-bold">
+            <h4 className="relative self-stretch mt-[-1.00px] font-heading-1-m font-[number:var(--heading-1-m-font-weight)] text-white text-[length:var(--heading-1-m-font-size)] tracking-[var(--heading-1-m-letter-spacing)] leading-[1.4] md:leading-[var(--heading-1-m-line-height)] [font-style:var(--heading-1-m-font-style)] text-sm md:text-5xl font-bold">
               Manage Daily Expenses With Your Flatmates.
-            </h1>
+            </h4>
           </div>
 
           <p className="relative self-stretch font-body-l font-[number:var(--body-l-font-weight)] text-white text-[length:var(--body-l-font-size)] tracking-[var(--body-l-letter-spacing)] leading-[1.6] md:leading-[var(--body-l-line-height)] [font-style:var(--body-l-font-style)] text-lg md:text-xl">
